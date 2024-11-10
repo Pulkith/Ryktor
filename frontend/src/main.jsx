ba2e1539eb7d85765181e718ed4d428ec1879fa4
Anjalee Narenthiren <anjaleen@wharton.upenn.edu>
2024-11-10 01:53:38 -0500

    Fix theme fix theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider theme={theme} initialColorMode="light" forcedColorMode="light">
-        <ColorModeScript initialColorMode="light" />
+    {/* <ChakraProvider theme={theme} initialColorMode="light" forcedColorMode="light"> */}
+      <ChakraProvider theme={theme}>
+        {/* <ColorModeScript initialColorMode="light" /> */}
         <App />
       </ChakraProvider>
     </BrowserRouter>
